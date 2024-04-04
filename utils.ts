2fae8de30f17775faf85442da10aafa030338a9b
Dean <deaneffi1414@gmail.com>
2024-04-05 01:51:37 +0300

    fix sorting to not work alphabetically

--- utils.ts
+++ utils.ts
@@ -6,6 +6,5 @@ export function localSave(tasks: Task[]) {
 }
 
 export function sortTasks(tasks: Task[]) {
-  tasks = tasks.sort((a, b) => a.name.localeCompare(b.name));
   return tasks.sort(a => (a.complete ? 1 : -1));
 }
